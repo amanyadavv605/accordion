9270fdccbea832917f38924c0b13e96cd8802ef7
Aman Yadav <amanmanohar011@gmail.com>
2025-05-16 22:27:25 +0530

    Starting Accordion project

---
+++ src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const Data = [
+    {
+      title: "What is Lorem Ipsum?",
+      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+      title: "Why do we use it?",
+      content: "It is a long established fact that a reader will be distracted by the readable content."
+    },
+    {
+      title: "Where does it come from?",
+      content: "Contrary to popular belief, Lorem Ipsum is not simply random text."
+    },
+    {
+      title: "Where can I get some?",
+      content: "There are many variations of passages of Lorem Ipsum available."
+    },
+    {
+      title: "Is it free to use?",
+      content: "Yes, Lorem Ipsum has been the industry's standard dummy text for many years."
+    }
+  ];
+
+export default Data;
